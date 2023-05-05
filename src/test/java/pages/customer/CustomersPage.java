917fd4c402460c19952dec024228e67345c3b951
amitbarel <amitbrl1797@gmail.com>
2023-05-05 12:58:28 +0300

    All pages

---
+++ src/test/java/pages/customer/CustomersPage.java
@@ -0,0 +1,51 @@
+package pages.customer;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import utils.Helper;
+
+public class CustomersPage {
+	private WebDriver driver;
+	private Helper helper;
+
+	@After
+	public void tearDown() {
+		// driver.quit();
+	}
+
+	@Before
+	public void setUp() throws IOException {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		helper = new Helper(driver);
+	}
+
+	@Test
+	public void testLogin() {
+		driver.get(helper.getBASE_URL() + "customer");
+		driver.manage().window().maximize();
+	}
+
+	public static void main(String args[]) {
+		JUnitCore junit = new JUnitCore();
+		junit.addListener(new TextListener(System.out));
+		org.junit.runner.Result result = junit.run(CustomersPage.class);
+
+		if (result.getFailureCount() > 0) {
+			System.out.println("Test failed.");
+			System.exit(1);
+		} else {
+			System.out.println("Test finished successfully.");
+			System.exit(0);
+		}
+	}
+}
---
+++ src/test/java/utils/Helper.java
@@ -0,0 +1,47 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Helper {
+	private final int DELAY_BIG = 10000, DELAY_MEDIUM = 7500, DELAY_SMALL = 5000;
+	private WebDriver driver;
+	private final String BASE_URL = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/";
+	
+	
+	public String getBASE_URL() {
+		return BASE_URL;
+	}
+
+
+	public Helper(WebDriver driver) {
+		this.driver = driver;
+	}
+
+
+	public ArrayList<String> usersList() {
+		driver.get(this.BASE_URL + "login");
+		ArrayList<String> names = new ArrayList<String>();
+		driver.findElement(By.xpath("//button[@ng-click = 'manager()']")).click();
+		driverWait(DELAY_MEDIUM);
+		driver.findElement(By.xpath("//button[@ng-class = 'btnClass3']")).click();
+		driverWait(DELAY_SMALL);
+		WebElement table = driver.findElement(By.xpath("//table[@class = 'table table-bordered table-striped']"));
+		List<WebElement> rows = table.findElements(By.cssSelector("tr.ng-scope"));
+		List<WebElement> columns = table.findElements(By.cssSelector("td.ng-binding"));
+		for (WebElement row : rows) {
+		    List<WebElement> cells = row.findElements(By.cssSelector("td.ng-binding"));
+		    names.add(cells.get(0).getText().concat(" "+cells.get(1).getText()));
+		}
+		return names;
+	}
+	
+	public void driverWait(int delay) {
+		driver.manage().timeouts().implicitlyWait(delay, TimeUnit.MILLISECONDS);
+	}
+}
